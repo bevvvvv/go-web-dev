3631b122803c2d9790e4f612f9646d57a71133ec
Joseph Sepich <computerwhiz1337@gmail.com>
2022-03-31 12:20:50 -0400

    faq

--- main.go
+++ main.go
@@ -18,10 +18,18 @@ func contact(w http.ResponseWriter, r *http.Request) {
 		href="mailto:support@lenslocked.com">support@lenslocked.com</a>.`)
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, `<h1>Frequently Asked Questions</h1>
+	<h3>Did you write this?</h3>
+	<p>Yes I did.</p>`)
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	// starts server -- my container exposes 9000 by default
 	http.ListenAndServe(":9000", r) // nil uses what is declared above
 }

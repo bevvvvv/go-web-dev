8901c133d784e9cf79a0f4ce46b9f41e7da3e814
Joseph Sepich <computerwhiz1337@gmail.com>
2022-04-07 07:56:16 -0400

    home view

--- main.go
+++ main.go
@@ -8,44 +8,20 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type User struct {
-	Name     string
-	Age      float64
-	Sports   []string
-	Children map[string]string
-}
+var homeTemplate *template.Template
 
 func home(w http.ResponseWriter, r *http.Request) {
 	// set content type
 	w.Header().Set("Content-Type", "text/html")
-
-	// load template
-	t, err := template.ParseFiles("templates/hello.gohtml")
-	if err != nil {
+	if err := homeTemplate.Execute(w, nil); err != nil {
 		panic(err)
 	}
-
-	// send data
-	data := User{
-		Name:     "John Smith",
-		Age:      34.5,
-		Sports:   []string{"Basketball", "Volleyball", "Baseball"},
-		Children: map[string]string{"Daughter": "Lisa", "Son": "Bradley"},
-	}
-	t.Execute(w, data)
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, `To get in touch, please send an email to <a 
 		href="mailto:support@lenslocked.com">support@lenslocked.com</a>.`)
-}
-
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, `<h1>Frequently Asked Questions</h1>
-	<h3>Did you write this?</h3>
-	<p>Yes I did.</p>`)
 }
 
 // custom NotFoundHandler
@@ -59,14 +35,18 @@ func (nf MyNotFound) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	<p>For assistance please <a href="/contact">contact support</a>.</p>`)
 }
 
-// http.HandlerFunc(function here) - can be used instead of defining custom class.
-
 func main() {
+	// load templates
+	var err error
+	homeTemplate, err = homeTemplate.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
 	// create mux router
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
 	r.NotFoundHandler = MyNotFound{}
 
 	// starts server -- my container exposes 9000 by default

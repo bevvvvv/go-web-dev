0bb2fd0092868bd8c57fabc4994b52d9318be1d0
Joseph Sepich <computerwhiz1337@gmail.com>
2022-04-07 08:03:51 -0400

    contact view

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeTemplate *template.Template
+var (
+	homeTemplate    *template.Template
+	contactTemplate *template.Template
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	// set content type
@@ -20,8 +23,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, `To get in touch, please send an email to <a 
-		href="mailto:support@lenslocked.com">support@lenslocked.com</a>.`)
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 // custom NotFoundHandler
@@ -39,6 +43,10 @@ func main() {
 	// load templates
 	var err error
 	homeTemplate, err = homeTemplate.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	contactTemplate, err = contactTemplate.ParseFiles("views/contact.gohtml")
 	if err != nil {
 		panic(err)
 	}

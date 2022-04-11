293b5a49654051ebbb1f27af9edce449106668f6
Joseph Sepich <computerwhiz1337@gmail.com>
2022-04-11 12:41:07 -0400

    create signup page

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 var (
 	homeView    *views.View
 	contactView *views.View
+	signupView  *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -20,15 +21,21 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	must(contactView.Render(w, nil))
 }
 
+func signup(w http.ResponseWriter, r *http.Request) {
+	must(signupView.Render(w, nil))
+}
+
 func main() {
 	// load views
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 
 	// create mux router
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/signup", signup)
 
 	// starts server -- my container exposes 9000 by default
 	http.ListenAndServe(":9000", r)

05938193a9d8731502c8027d1308327ca27bc22d
Joseph Sepich <computerwhiz1337@gmail.com>
2022-05-03 17:14:36 -0400

    test file

---
+++ models/users_test.go
@@ -0,0 +1,53 @@
+package models
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func testingUserService() (*UserService, error) {
+	const (
+		host     = "host.docker.internal"
+		port     = 5432
+		user     = "postgres"
+		password = "secretpass"
+		dbname   = "fakeoku_test"
+	)
+
+	connectionInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	userService, err := NewUserService(connectionInfo)
+	if err != nil {
+		return nil, err
+	}
+	userService.db.LogMode(false)
+
+	// Clear table
+	userService.DestructiveReset()
+	return userService, nil
+}
+
+func TestCreateUser(t *testing.T) {
+	userService, err := testingUserService()
+	if err != nil {
+		t.Fatal(err)
+	}
+	user := User{
+		Name:  "Michael Scott",
+		Email: "michael@dundermifflin.net",
+	}
+	err = userService.Create(&user)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if user.ID == 0 {
+		t.Errorf("Expected ID > 0. Receieved %d", user.ID)
+	}
+	if time.Since(user.CreatedAt) > time.Duration(5*time.Second) {
+		t.Errorf("Expected recent creation. Received %s", user.CreatedAt)
+	}
+	if time.Since(user.UpdatedAt) > time.Duration(5*time.Second) {
+		t.Errorf("Expected recent creation. Received %s", user.UpdatedAt)
+	}
+}
---
+++ psql.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker exec -it -u postgres pg psql
+
+# CREATE DATABASE fakeoku_test;
+# CREATE DATABASE fakeoku;
